4c64fbfac47a8ec2dcc95a49bb55dfd232333075
Colten Larsen <larsen.colten@gmail.com>
2020-03-07 11:07:30 -0700

    Update apriori.R

--- Apriori/apriori.R
+++ Apriori/apriori.R
@@ -2,17 +2,24 @@
 library(arules);
 data(Groceries);
 
-frequentItems <- eclat (Groceries, parameter = list(supp = 0.07, maxlen = 15)) # calculates support for frequent items
+frequentItems <- eclat (Groceries, parameter = list(supp = 0.07, maxlen = 15))
 inspect(frequentItems)
 
 rules <- apriori (Groceries, parameter = list(supp = 0.001, conf = 0.5))
 
-rules_conf <- sort (rules, by="confidence", decreasing=TRUE) # 'high-confidence' rules.
+rules_conf <- sort (rules, by="confidence", decreasing=TRUE)
 inspect(head(rules_conf))
 
-rules_lift <- sort (rules, by="lift", decreasing=TRUE) # 'high-lift' rules.
+rules_lift <- sort (rules, by="lift", decreasing=TRUE)
 inspect(head(rules_lift))
 
-rules <- apriori (data=Groceries, parameter=list (supp=0.001,conf = 0.15,minlen=2), appearance = list(default="rhs",lhs="soda"), control = list (verbose=F)) # those who bought 'milk' also bought..
-rules_conf <- sort (rules, by="count", decreasing=TRUE) # 'high-confidence' rules.
+rules <- apriori (data=Groceries, parameter=list (supp=0.001,conf = 0.15,minlen=2), appearance = list(default="rhs",lhs="shopping bags"), control = list (verbose=F))
+rules_conf <- sort (rules, by="conf", decreasing=TRUE)
 inspect(head(rules_conf))
+
+
+# {soda} => {other vegetables}
+# {yogurt} => {root vegetables}
+# {canned fish,hygiene articles} => {whole milk}
+# {sausage} => {shopping bags}
+# {shopping bags} => {soda}
