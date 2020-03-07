6f68a226e6a238005e33065dc4c5b8a67629c82f
Colten Larsen <larsen.colten@gmail.com>
2020-03-07 11:12:31 -0700

    Update apriori.R

--- Apriori/apriori.R
+++ Apriori/apriori.R
@@ -2,10 +2,11 @@
 library(arules);
 data(Groceries);
 
-frequentItems <- eclat (Groceries, parameter = list(supp = 0.07, maxlen = 15))
+frequentItems <- eclat (Groceries, parameter = list(supp = 0.1, maxlen = 15))
 inspect(frequentItems)
 
-rules <- apriori (Groceries, parameter = list(supp = 0.001, conf = 0.5))
+rules_supp <- sort (rules, by="support", decreasing=TRUE)
+inspect(head(rules_supp))
 
 rules_conf <- sort (rules, by="confidence", decreasing=TRUE)
 inspect(head(rules_conf))
@@ -13,11 +14,7 @@ inspect(head(rules_conf))
 rules_lift <- sort (rules, by="lift", decreasing=TRUE)
 inspect(head(rules_lift))
 
-rules <- apriori (data=Groceries, parameter=list (supp=0.001,conf = 0.15,minlen=2), appearance = list(default="rhs",lhs="shopping bags"), control = list (verbose=F))
-rules_conf <- sort (rules, by="conf", decreasing=TRUE)
-inspect(head(rules_conf))
-
-
+# Interesting rules
 # {soda} => {other vegetables}
 # {yogurt} => {root vegetables}
 # {canned fish,hygiene articles} => {whole milk}
